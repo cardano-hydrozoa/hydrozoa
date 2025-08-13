56bfa41e9387bd96293581323d17afb8ff147278
otto <sae3023@gmail.com>
2025-08-13 16:23:40 +0200

    init tx rewrite

--- src/main/scala/hydrozoa/l1/multisig/tx/initialization/ScalusInitializationTxBuilder.scala
+++ src/main/scala/hydrozoa/l1/multisig/tx/initialization/ScalusInitializationTxBuilder.scala
@@ -13,20 +13,19 @@ import scalus.cardano.address.ShelleyDelegationPart.Null
 import scalus.cardano.address.{Address, ShelleyAddress, ShelleyPaymentPart}
 import scalus.cardano.ledger.*
 import scalus.cardano.ledger.DatumOption.Inline
+import scalus.cardano.ledger.txbuilder.BuilderContext
 
 import scala.collection.immutable.SortedMap
 
-class ScalusInitializationTxBuilder(backendService: BackendService) extends InitTxBuilder {
+class ScalusInitializationTxBuilder(backendService: BackendService, builderContext: BuilderContext)
+    extends InitTxBuilder {
 
     override def mkInitializationTxDraft(
         recipe: InitTxRecipe
     ): Either[String, (InitTx, AddressBechL1)] =
         bloxToScalusUtxoQuery(backendService, recipe.seedUtxo.toScalus) match {
             case Left(err) => Left("Scalus InititializationTxBuilder failed: " ++ err)
-            case Right(seedOutput) =>
-                Right({
-                    // TODO: we set the fee to 1 ada, but this doesn't need to be
-                    val feeCoin = Coin(1_000_000)
+            case Right(seedOutput) => Right({
 
                     // Construct head native script directly from the list of peers
                     val headNativeScript = mkHeadNativeScript(recipe.peers)
@@ -69,49 +68,16 @@ class ScalusInitializationTxBuilder(backendService: BackendService) extends Init
                     // and beacon, as well as the initial datum.
                     val headValue: Value =
                         Value(coin = Coin(recipe.coins.toLong), multiAsset = beaconToken)
-                    val headOutput: TransactionOutput =
-                        TransactionOutput(
-                          address = headAddress,
-                          value = headValue,
-                          datumOption = Some(Inline(mkInitMultisigTreasuryDatum.toData))
-                        )
 
-                    val changeOutput: TransactionOutput = TransactionOutput(
-                      address = seedOutput.address,
-                      // Change is calculated manually here as the seed output's value, minus the
-                      // ada put into the head, minus the fee.
-                      value =
-                          try {
-                              seedOutput.value -
-                                  Value(coin = Coin(recipe.coins.toLong)) -
-                                  Value(
-                                    coin = feeCoin
-                                  )
-                          } catch {
-                              case _: IllegalArgumentException =>
-                                  return Left("illegal change value found")
-                          },
-                      datumOption = None
-                    )
-
-                    val ourBody =
-                        emptyTxBody.copy(
-                          inputs = Set(recipe.seedUtxo.toScalus),
-                          outputs = IndexedSeq(headOutput, changeOutput).map(Sized(_)),
-                          // TODO: we set the fee to 1 ada, but this doesn't need to be
-                          fee = feeCoin,
-                          mint = Some(Mint(beaconToken))
-                        )
-
-                    val scalusTransaction: Transaction = Transaction(
-                      body = KeepRaw(ourBody),
-                      witnessSet = TransactionWitnessSet(nativeScripts = Set(headNativeScript)),
-                      isValid = true,
-                      auxiliaryData = None
-                    )
+                    val tx =
+                        builderContext
+                            .withUtxo(Map(recipe.seedUtxo.toScalus -> seedOutput))
+                            .buildNewTx
+                            .payAndMint(headAddress, headValue)
+                            .doFinalize
 
                     (
-                      Tx(Cbor.encode(scalusTransaction).toByteArray),
+                      Tx(Cbor.encode(tx).toByteArray),
                       headAddress match {
                           case sa: ShelleyAddress => AddressBech(sa.toBech32.get)
                           // NOTE (Peter, 2025-08-07) I miss monads, how do I do those in scala?
