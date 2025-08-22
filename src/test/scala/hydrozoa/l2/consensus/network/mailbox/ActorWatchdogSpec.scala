b454d4d4c361de045164bbe9ec9a1787b6a0601c
Ilia Rodionov <hey@euony.me>
2025-08-22 13:52:45 -0600

    Chore(watchdog): polish "Watchdog works" test

--- src/test/scala/hydrozoa/l2/consensus/network/mailbox/ActorWatchdogSpec.scala
+++ src/test/scala/hydrozoa/l2/consensus/network/mailbox/ActorWatchdogSpec.scala
@@ -8,17 +8,16 @@ import scala.concurrent.duration.DurationInt
 
 class ActorWatchdogSpec extends ScalaCheckSuite:
 
-    class WatchdogCounter extends Watchdog:
-        private var counter = 0L
-        override def wakeUp() = counter = counter + 1
-        def getCounter: Long = counter
-
     test("Watchdog works"):
+
+        class WatchdogCounter extends Watchdog:
+            var counter = 0L
+            override def wakeUp(): Unit = counter += 1
 
         supervised {
             given timer: WatchdogTimeoutSeconds = WatchdogTimeoutSeconds(1)
             val actor: ActorRef[WatchdogCounter] = ActorWatchdog.create(WatchdogCounter())
 
             sleep(2500.millis)
-            assertEquals(actor.ask(_.getCounter), 2L)
+            assertEquals(actor.ask(_.counter), 2L)
         }
