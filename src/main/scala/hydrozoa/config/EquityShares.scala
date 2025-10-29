b657308e8214622621cee9429e125c0502347fcc
Ilia Rodionov <hey@euony.me>
2025-10-28 22:41:43 -0600

    Feat: builder for multisig deinit tx

--- src/main/scala/hydrozoa/config/EquityShares.scala
+++ src/main/scala/hydrozoa/config/EquityShares.scala
@@ -2,28 +2,31 @@ package hydrozoa.config
 
 import hydrozoa.AddressL1
 import scalus.cardano.ledger.value.Coin
-import spire.compat.integral
+import scalus.cardano.ledger.value.Coin.Unbounded
+import spire.compat.numeric
 import spire.implicits.additiveSemigroupOps
 import spire.math.Number.apply
 import spire.math.{Rational, UByte}
 import spire.syntax.literals.r
 
-case class PeerShare(
-    payoutAddress: AddressL1,
-    equityShare: Rational,
-    fallbackDeposit: Coin
-)
-
-/** TODO: revise
-  *
-  * This represents the head's distributable liabilities due to shareholders (peers):
-  *   - collateral deposits (used to provide a collateral utxos for peers upon switching to the
-  *     rule-based regime)
-  *   - vote deposits (used to fund vote utxos)
-  *   - equity shares
+/** This represents the head's distributable liabilities due to shareholders (peers), see
+  * [[PeerEquityShare]].
   */
 case class EquityShares private (
-    private val peerShares: Map[UByte, PeerShare]
+    private val peerShares: Map[UByte, PeerEquityShare]
+) {
+    def totalFallbackDeposit: Coin =
+        peerShares.values.map(_.fallbackDeposit).sumCoins.unsafeToCoin
+}
+
+/** Peers's equity share and deposit. */
+case class PeerEquityShare(
+    // Address to pay out liabilities
+    payoutAddress: AddressL1,
+    // Peer's share of equity
+    equityShare: Rational,
+    // Peer's total deposit, stored in multisig utxo, the collective part depends on the equityShare
+    fallbackDeposit: Coin
 )
 
 object EquityShares:
@@ -49,7 +52,7 @@ object EquityShares:
                         (fallbackTxFee + defaultVoteDeposit).scaleFractional(share)
                     val fallbackDeposit =
                         (collectiveContingencyShare + collateralUtxo + voteUtxo).unsafeToCoin()
-                    PeerShare(address, share, fallbackDeposit)
+                    PeerEquityShare(address, share, fallbackDeposit)
                 })
                 .toMap
 
@@ -70,34 +73,40 @@ object EquityShares:
     /** Distribution of equity and deposits for multisig regime
       */
     object MultisigRegimeDistribution:
-        extension (self: EquityShares)
-            def distribute(equity: Coin): Distribution = {
 
-                // (AddressL1, (fallbackDeposit, equity payouts))
-                val payoutsParts: Iterable[(AddressL1, (Coin, Coin))] =
-                    self.peerShares.values.map(v =>
-                        v.payoutAddress -> (
-                          v.fallbackDeposit,
-                          equity
-                              .scaleFractional(v.equityShare)
-                              // TODO: RoundingMode.DOWN
-                              .unsafeToCoin()
-                        )
+        /** @param equity
+          *   total equity (not to be confused with residual treasury that contains the deposits
+          *   after the finalization tx)
+          * @return
+          *   distribution of funds
+          */
+        def distribute(equityShares: EquityShares)(equity: Coin): Distribution = {
+
+            // (AddressL1, (fallback deposit, equity share))
+            val payoutsParts: Iterable[(AddressL1, (Coin, Coin))] =
+                equityShares.peerShares.values.map(v =>
+                    v.payoutAddress -> (
+                      v.fallbackDeposit,
+                      equity
+                          .scaleFractional(v.equityShare)
+                          // TODO: RoundingMode.DOWN
+                          .unsafeToCoin()
                     )
+                )
 
-                val equityPayoutsTotal =
-                    payoutsParts
-                        .foldLeft(Coin.Unbounded.zero)((acc, p) => acc + p._2._2)
-                        .unsafeToCoin
+            val equityPayoutsTotal =
+                payoutsParts
+                    .foldLeft(Unbounded.zero)((acc, p) => acc + p._2._2)
+                    .unsafeToCoin
 
-                val dust = (equity - equityPayoutsTotal).unsafeToCoin
+            val dust = (equity - equityPayoutsTotal).unsafeToCoin
 
-                val payouts = payoutsParts
-                    .map((a, p) => a -> (p._1 + p._2).unsafeToCoin)
-                    .toMap
+            val payouts = payoutsParts
+                .map((a, p) => a -> (p._1 + p._2).unsafeToCoin)
+                .toMap
 
-                Distribution(payouts, dust)
-            }
+            Distribution(payouts, dust)
+        }
 
     object RuleBasedRegimeDistribution:
         extension (self: EquityShares)
@@ -110,7 +119,8 @@ object EquityShares:
                             // TODO: RoundingMode.DOWN
                             .unsafeToCoin()
                 )
-                val equityPayoutsTotal = payouts.foldLeft(Coin.Unbounded.zero)((acc, p) => acc + p._2).unsafeToCoin
+                val equityPayoutsTotal =
+                    payouts.foldLeft(Coin.Unbounded.zero)((acc, p) => acc + p._2).unsafeToCoin
                 val dust = (equity - equityPayoutsTotal).unsafeToCoin
                 Distribution(payouts.toMap, dust)
             }
--- src/main/scala/hydrozoa/multisig/ledger/dapp/tx/DeinitTx.scala
+++ src/main/scala/hydrozoa/multisig/ledger/dapp/tx/DeinitTx.scala
@@ -1,31 +1,178 @@
 package hydrozoa.multisig.ledger.dapp.tx
 
+import hydrozoa.config.EquityShares
+import hydrozoa.config.EquityShares.MultisigRegimeDistribution
+import hydrozoa.multisig.ledger.dapp.tx.Tx.Builder.{BuildErrorOr, explain}
 import hydrozoa.multisig.ledger.dapp.utxo.TreasuryUtxo
-import scalus.cardano.ledger.Transaction
+import monocle.Focus.focus
+import scala.Function.const
+import scalus.cardano.ledger.value.Coin
+import scalus.cardano.ledger.value.Coin.{ArithmeticError, Unbounded}
+import scalus.cardano.ledger.{Coin as OldCoin, KeepRaw, Sized, Transaction, TransactionOutput, Value}
+import scalus.cardano.txbuilder.*
+import scalus.cardano.txbuilder.TransactionBuilderStep.*
+import spire.implicits.additiveGroupOps
 
 /** The Deinit tx in the multisig regime acts mostly the same way the eponymous tx in the rule-based
   * regime:
   *   - It spends the empty treasury utxo (combined with the multisig utxo by the finalization tx)
-  *     that contains peers' collateral and voting deposits, collective deposits, and the head
-  *     equity.
+  *     that contains peers' deposits, and the remaining head equity.
   *   - It pays out to every peer as one output (the deposits guarantee that every payout is at
-  *     least > 2 * minAda):
+  *     least >= 2 * minAda):
   *     - Their collateral deposit
   *     - Their voting deposit
-  *     - Their share of collective deposits (according to equity shares)
+  *     - Their share of fallback deposits (according to equity shares)
   *     - Their share of equity
-  *   - All those payouts will fit the single tx. This holds, because the number of peers is limited
-  *     by the fallback tx that should be able _simultaneously_ distribute collaterals and (N+1)
-  *     vote utxos in one go, the 2x+1 number of outputs.
+  *   - All those payouts will always fit the single tx. This holds, because the number of peers is
+  *     limited by the fallback tx that should be able _simultaneously_ distribute collaterals and
+  *     (N+1) vote utxos in one go, the 2x+1 number of outputs.
+  *   - The fees are paid from the treasury, reducing the equity. To calculate the fees the outputs
+  *     amounts recalculated after the fees are calculated.
   */
 case class DeinitTx(
-    override val tx: Transaction,
-    override val treasurySpent: TreasuryUtxo
+    override val treasurySpent: TreasuryUtxo,
+    override val tx: Transaction
 ) extends Tx,
       TreasuryUtxo.Spent
 
 object DeinitTx:
 
-    final case class Builder(config: Tx.Builder.Config)
+    final case class Builder(
+        treasuryToSpend: TreasuryUtxo,
+        equityShares: EquityShares,
+        config: Tx.Builder.Config
+    ) {
+        def postProcess(ctx: TransactionBuilder.Context): DeinitTx =
+            DeinitTx(treasuryToSpend, ctx.transaction)
+
+        private val steps = Steps(treasuryToSpend, equityShares, config)
+
+        def build: BuildErrorOr[DeinitTx] = for {
+            payouts <- steps.mkPayouts.left
+                .map(s =>
+                    // Use tx-specific errors
+                    SomeBuildError.BalancingError.apply(
+                      TxBalancingError.Failed(IllegalStateException(s))
+                    )
+                )
+                .explain(const("Could not distribute equity"))
+
+            ctx <- TransactionBuilder
+                .build(
+                  config.env.network,
+                  steps.commonSteps ++ payouts
+                )
+                .explain(const("Could not build deinit transaction"))
+
+            res <- ctx
+                .finalizeContext(
+                  config.env.protocolParams,
+                  SharePayoutsDiffHandler.handler(treasuryToSpend, equityShares),
+                  config.env.evaluator,
+                  config.validators
+                )
+                .explain(const("Could not balance deinit transaction"))
+        } yield postProcess(res)
+    }
+
+    private case class Steps(
+        treasuryToSpend: TreasuryUtxo,
+        equityShares: EquityShares,
+        config: Tx.Builder.Config
+    ) {
+
+        def commonSteps: List[TransactionBuilderStep] =
+            List(
+              stepReferenceHNS,
+              spendTreasury
+            ) ++ burnHeadTokens
+
+        private def stepReferenceHNS =
+            ReferenceOutput(config.headNativeScriptReferenceInput)
+
+        private def spendTreasury =
+            Spend(treasuryToSpend.asUtxo, config.headNativeScript.witness)
+
+        private def burnHeadTokens = {
+            List(
+              Mint(
+                config.headNativeScript.policyId,
+                treasuryToSpend.headTokenName,
+                -1L,
+                config.headNativeScript.witness
+              )
+              // TODO: burn witness token
+            )
+        }
+
+        def mkPayouts: Either[String, List[TransactionBuilderStep]] = {
+            // FIXME: remove later
+            val treasuryVCoin = Coin.unsafeApply(treasuryToSpend.value.coin.value)
+
+            val distribute = MultisigRegimeDistribution.distribute(equityShares)
+
+            for {
+                equity <- (treasuryVCoin - equityShares.totalFallbackDeposit).toCoin match
+                    case Left(ArithmeticError.Underflow) =>
+                        Left("residual treasury can't be less then total deposits")
+                    case Right(equity) => Right(equity)
+
+                distribution = distribute(equity)
+
+                outputs = distribution.payouts
+                    .map(p => Send(TransactionOutput.apply(p._1, Value.lovelace(p._2.underlying))))
+                    .toList
+
+                fee = Fee(OldCoin.apply(distribution.dust.underlying))
+
+            } yield outputs :+ fee
+        }
+    }
+
+    /** A handler that re-distributes diff amount over all shares. Outputs (and optionally fee) are
+      * replaced.
+      */
+    object SharePayoutsDiffHandler:
+        def handler(treasuryToSpend: TreasuryUtxo, equityShares: EquityShares): DiffHandler = (
+            diff: Long,
+            tx: Transaction
+        ) => {
+
+            // FIXME: remove later
+            val treasuryVCoin = Unbounded.apply(treasuryToSpend.value.coin.value)
+
+            val distribute = MultisigRegimeDistribution.distribute(equityShares)
+            val diffCoin = Coin.unsafeApply(diff)
+
+            for {
+                equity <-
+                    (treasuryVCoin - (equityShares.totalFallbackDeposit + diffCoin)).toCoin match
+                        case Left(ArithmeticError.Underflow) =>
+                            Left(
+                              TxBalancingError.Failed(
+                                IllegalStateException(
+                                  "residual treasury can't be less then total deposits"
+                                )
+                              )
+                            )
+                        case Right(equity) => Right(equity)
+
+                distribution = distribute(equity)
+
+                outputs = distribution.payouts
+                    .map(p => Sized(TransactionOutput.apply(p._1, Value.lovelace(p._2.underlying))))
+                    .toList
+
+            } yield {
+                val tb = tx.body.value
+                    .focus(_.outputs)
+                    .replace(IndexedSeq.from(outputs))
+                    .focus(_.fee)
+                    .modify(fee => OldCoin.apply(fee.value + distribution.dust.underlying))
+                val t = tx.copy(body = KeepRaw(tb))
+                t
+            }
+
+        }
 
 end DeinitTx
