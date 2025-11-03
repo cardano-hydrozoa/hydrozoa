735994753c41938eb7a1f652bf8159529c1e062d
Ilia Rodionov <hey@euony.me>
2025-11-03 17:30:39 -0600

    Chore: temporarily diable withdraw tx test, fix dispute script test

--- src/test/scala/hydrozoa/rulebased/ledger/dapp/script/plutus/DisputeResolutionScriptTest.scala
+++ src/test/scala/hydrozoa/rulebased/ledger/dapp/script/plutus/DisputeResolutionScriptTest.scala
@@ -15,14 +15,14 @@ class DisputeResolutionScriptTest extends AnyFunSuite {
     test("Script compiles, size and hash is still the same") {
 
         assertResult(
-          ScriptHash.fromHex("4d09665e6f52af8ce9b3ff38b3d59ea58218b3e652c867fe1c90e63e"),
+          ScriptHash.fromHex("4de666d32717870dd7b68eefcfc40c78c0b20abb5067d127e0929b20"),
           "Script hash should be stable. In case the script is modified or Scalus is bumped please update the test."
         ) {
             DisputeResolutionScript.compiledScriptHash
         }
 
         assertResult(
-          13383,
+          13696,
           "Script size should be stable. In case the script is modified please update the test."
         ) {
             DisputeResolutionScript.flatEncoded.length
