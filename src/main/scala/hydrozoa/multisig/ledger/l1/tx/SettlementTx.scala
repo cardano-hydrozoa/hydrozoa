5aa8709dc95b10058fccbbf44505c74155086594
George Flerovsky <george@flerovsky.ca>
2026-03-16 18:14:49 -0400

    Feat: absorb non-ADA from deposits in settlement tx

--- src/main/scala/hydrozoa/multisig/ledger/l1/tx/SettlementTx.scala
+++ src/main/scala/hydrozoa/multisig/ledger/l1/tx/SettlementTx.scala
@@ -240,6 +240,8 @@ private object SettlementTxOps {
                 Spend(deposit.toUtxo, config.headMultisigScript.witnessAttached)
 
             private val spendDeposits: List[Spend] = depositsToSpend.toList.map(mkDepositStep)
+            
+            private val valueAbsorbed: Value = depositsToSpend.map(_.l2Value).foldLeft(Value.zero)(_ + _)
 
             /////////////////////////////////////////////////////////
             // Send rollout (maybe)
@@ -269,7 +271,7 @@ private object SettlementTxOps {
             /////////////////////////////////////////////////////////
             // Send treasury
             private val treasuryOutputValue: Value =
-                mbRolloutValue.fold(treasuryToSpend.value)(treasuryToSpend.value - _)
+                valueAbsorbed + mbRolloutValue.fold(treasuryToSpend.value)(treasuryToSpend.value - _)
 
             private val treasuryOutput: TxOutput.Babbage = {
                 TxOutput.Babbage(
