46e3e025bbbf510f459019a419a464fc8848b2d0
Eric Torreborre <etorreborre@yahoo.com>
2026-04-24 10:21:45 +0200

    test: migrate value/Gen.scala to registry-scalacheck
    Signed-off-by: Eric Torreborre <etorreborre@yahoo.com>

--- src/test/scala/hydrozoa/lib/cardano/value/Gen.scala
+++ src/test/scala/hydrozoa/lib/cardano/value/Gen.scala
@@ -1,35 +1,37 @@
 package hydrozoa.lib.cardano.value
 
 import hydrozoa.lib.cardano.value.coin.Coin
-import hydrozoa.lib.cardano.value.coin.Coin.Unbounded
 import hydrozoa.lib.cardano.value.multiasset.MultiAsset
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen as Gen0
 
 import coin.Gen.Arb.given
 import multiasset.Gen.Arb.given
 
+import registry.*
+import registry.scalacheck.*
+
 object Gen {
+    private val registry =
+        genFun[Value] +:
+            genFun[Value.Unbounded] +:
+            genFun[Value.Fractional] +:
+            value(arbitrary[Coin]: Gen0[Coin]) +:
+            value(arbitrary[Coin.Unbounded]: Gen0[Coin.Unbounded]) +:
+            value(arbitrary[Coin.Fractional]: Gen0[Coin.Fractional]) +:
+            value(arbitrary[MultiAsset]: Gen0[MultiAsset]) +:
+            value(arbitrary[MultiAsset.Unbounded]: Gen0[MultiAsset.Unbounded]) +:
+            value(arbitrary[MultiAsset.Fractional]: Gen0[MultiAsset.Fractional])
+
     object Arb {
-        implicit val valueArb: Arbitrary[Value] = Arbitrary(
-          for {
-              coin <- arbitrary[Coin]
-              ma <- arbitrary[MultiAsset]
-          } yield Value(coin, ma)
-        )
+        implicit val valueArb: Arbitrary[Value] =
+            Arbitrary(registry.make[Gen0[Value]])
 
-        implicit val valueUnboundedArb: Arbitrary[Value.Unbounded] = Arbitrary(
-          for {
-              coin <- arbitrary[Unbounded]
-              ma <- arbitrary[MultiAsset.Unbounded]
-          } yield Value.Unbounded(coin, ma)
-        )
+        implicit val valueUnboundedArb: Arbitrary[Value.Unbounded] =
+            Arbitrary(registry.make[Gen0[Value.Unbounded]])
 
-        implicit val valueFractionalArb: Arbitrary[Value.Fractional] = Arbitrary(
-          for {
-              coin <- arbitrary[Coin.Fractional]
-              ma <- arbitrary[MultiAsset.Fractional]
-          } yield Value.Fractional(coin, ma)
-        )
+        implicit val valueFractionalArb: Arbitrary[Value.Fractional] =
+            Arbitrary(registry.make[Gen0[Value.Fractional]])
     }
 }
