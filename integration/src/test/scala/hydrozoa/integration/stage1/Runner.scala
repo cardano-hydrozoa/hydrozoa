1616d67fc1d17f2fced2f6f6053772c924a09c6b
Ilia Rodionov <hey@euony.me>
2026-05-09 15:20:41 -0600

    Chore: fix the stage1 runner

--- integration/src/test/scala/hydrozoa/integration/stage1/Runner.scala
+++ integration/src/test/scala/hydrozoa/integration/stage1/Runner.scala
@@ -17,13 +17,13 @@ object Stage1PropertiesL1Mock extends YetAnotherProperties("Integration Stage 1 
         p: org.scalacheck.Test.Parameters
     ): org.scalacheck.Test.Parameters = {
         p.withWorkers(1)
-            // .withPropFilter(Some("Block promotion with real L2 txs"))
+            //.withPropFilter(Some("Block promotion with real L2 txs"))
             //.withPropFilter(Some("Dusty head finalization"))
             //.withPropFilter(Some("Ongoing withdrawals"))
-//.withPropFilter(Some("Deposits"))
-//.withInitialSeed(
-              Some(Seed.fromBase64("lr7yvfMC6Qxtovs5UjWZSfTcqzr7pHrzQ4_mcWApnUP=").get)
-            )
+            //.withPropFilter(Some("Deposits"))
+            //.withInitialSeed(
+            //  Some(Seed.fromBase64("lr7yvfMC6Qxtovs5UjWZSfTcqzr7pHrzQ4_mcWApnUP=").get)
+            //)
         // NB: careful, this will override -s from the command line
         // .withMinSuccessfulTests(100) // 10000
         // .withMaxSize(100) // 500
